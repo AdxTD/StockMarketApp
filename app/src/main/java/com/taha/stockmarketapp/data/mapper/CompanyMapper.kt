c6c3095b837ac854ee688de2d4a13f3c6c9f59d3
TahaDarwishIT <taha.darwish@admix.in>
2024-03-26 18:30:03 +0100

    *Project initial structure *Created StockAPI for data remote fetch *Created Room DB interface for local data caching *Created CompanyListing Entity and Model, plus mapper

---
+++ app/src/main/java/com/taha/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,23 @@
+package com.taha.stockmarketapp.data.mapper
+
+import com.taha.stockmarketapp.data.local.CompanyListingEntity
+import com.taha.stockmarketapp.domain.model.CompanyListing
+
+class CompanyMapper {
+
+    fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+        return CompanyListing(
+            name = name,
+            symbol = symbol,
+            exchange = exchange
+        )
+    }
+
+    fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+        return CompanyListingEntity(
+            name = name,
+            symbol = symbol,
+            exchange = exchange
+        )
+    }
+}

3f8a9af8b2a5637ce0fefb7486b5c576c9003d15
AdxTD <taha.darwish.it@gmail.com>
2024-05-20 15:41:38 +0200

    *Created and configured Dagger Hilt for DI *Fixed some issues around multiple files

---
+++ app/src/main/java/com/taha/stockmarketapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.taha.stockmarketapp.di
+
+import com.taha.stockmarketapp.data.csv.CSVParser
+import com.taha.stockmarketapp.data.csv.CompanyListingsParser
+import com.taha.stockmarketapp.data.repository.StockRepositoryImpl
+import com.taha.stockmarketapp.domain.model.CompanyListing
+import com.taha.stockmarketapp.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
